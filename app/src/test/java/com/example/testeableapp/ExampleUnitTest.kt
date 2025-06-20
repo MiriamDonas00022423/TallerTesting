19b2598279266ec6519376736e900e47bc112b51
MiriamDonas00022423 <00022423@uca.edu.sv>
2025-06-20 13:35:20 -0600

    Secondparttesting

--- app/src/test/java/com/example/testeableapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testeableapp/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.testeableapp
 
+import com.example.testeableapp.ui.Screens.calculateTip
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +12,66 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun unitTestOne() {
+        val amount = 200.75
+        val tipPercent = 37
+        val roundUp = true
+        val result = calculateTip(amount, tipPercent, roundUp)
+        val delta = 0.01
+
+        assertEquals(75.0, result, delta)
     }
+
+    @Test
+    fun secondUnitTest() {
+
+        val amount = -50.0
+        val tipPercent = 20
+        val roundUp = false
+        val result = calculateTip(amount, tipPercent, roundUp)
+        val delta = 0.01
+
+
+        assertEquals("La propina debe ser 0.0 para montos negativos", 0.0, result, delta)
+    }
+
+    @Test
+    fun thirdUnitTest() {
+        val amount = 200.0
+        val tipPercent = 37
+        val roundUp = true
+        val numberOfPeople = 4
+        val tip = calculateTip(amount, tipPercent, roundUp)
+        val totalAmount = amount + tip
+        val expectedTotalPerPerson = totalAmount / numberOfPeople
+        val result = (amount + tip) / numberOfPeople
+        val delta = 0.01
+        assertEquals(expectedTotalPerPerson, result, delta)
+    }
+
+    @Test
+    fun testEmptyBillAmount() {
+        val billAmount = ""
+        val amount = billAmount.toDoubleOrNull() ?: 0.0
+        assertEquals(0.0, amount, 0.01)
+    }
+
+    @Test
+    fun testNonNumericBillAmountReturnsZero() {
+        val billAmount = "abc"
+        val amount = billAmount.toDoubleOrNull() ?: 0.0
+        assertEquals(0.0, amount, 0.01)
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
